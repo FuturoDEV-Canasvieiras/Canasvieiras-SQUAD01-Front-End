e8657b911ba6f756575a959a210ee4c252609883
Gabriela Riedel <gabrielariedel1@gmail.com>
2023-06-22 22:22:53 -0300

    refatorei com validação o cadastro armazem

--- DEVin-Adotion/src/componentes/formularios/CadastroArmazemForm/CadastroArmazemForm.jsx
+++ DEVin-Adotion/src/componentes/formularios/CadastroArmazemForm/CadastroArmazemForm.jsx
@@ -1,20 +1,31 @@
 import { useFetch } from '../../../hooks/useFetch'
 import { useForm } from '../../../hooks/useForm'
-//url do fetch = mockapi pra testes
+
 export default function CadastroArmazemForm() {
     const { handleChange, form, resetForm } = useForm({ nome: "", animal: "", situacao: true })
     const { createData } = useFetch("http://localhost:8080/armazem")
 
     const handleSubmit = () => {
-        createData(form)
-        resetForm();
+        if (form.nome.trim().length === 0) {
+            alert("O nome do armazém não pode ser vazio ou em branco");
+            return;
+          }
+          createData(form);
+          resetForm()
     }
+
     return (
         <>
             <h1>Cadastro Armazém</h1>
             <form onSubmit={handleSubmit}>
                 Armazém:<br />
-                <input type="text" name="nome" value={form.nome} onChange={handleChange} /><br />
+                <input
+                    type="text"
+                    name="nome"
+                    value={form.nome}
+                    onChange={handleChange}
+                    minLength={3}
+                /><br />
                 <label htmlFor="animal">Animal:</label><br />
                 <select name="animal" value={form.animal} onChange={handleChange}>
                     <option value="" disabled>Selecione o Estoque</option>
