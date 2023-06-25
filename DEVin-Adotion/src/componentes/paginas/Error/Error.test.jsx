370009423462625d414b6063a94ce74a11d40a68
Gabriela Vasco <gabrielavs1226@gmail.com>
2023-06-25 14:10:40 -0300

    test: página de erro

---
+++ DEVin-Adotion/src/componentes/paginas/Error/Error.test.jsx
@@ -0,0 +1,21 @@
+import { render, screen } from "@testing-library/react";
+import { vi } from "vitest";
+import { BrowserRouter } from "react-router-dom";
+import Error from "./Error";
+
+describe("Error", () => {
+    beforeEach(() => {
+        vi.resetAllMocks();
+    });
+    
+    vi.mock("../../../hooks/useFetch");
+    
+    test("Deve renderizar o componente corretamente", () => {
+        render(
+        <BrowserRouter>
+            <Error />
+        </BrowserRouter>
+        );
+    });
+    
+});
